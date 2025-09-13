e354b3b3f44c6252a2c964b5d5126382868f8878
Antel0pe <anishphadnis.ca@gmail.com>
2025-09-13 17:25:58 -0400

    delete past attempts

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,7 @@
 import {useMemo, useState} from "react";
 import dynamic from "next/dynamic";
 
-// const CesiumMap = dynamic(() => import("./components/cesium_map"), { ssr: false });
 const TimeSlider = dynamic(() => import("./components/time_slider"), { ssr: false });
-const HeightMesh = dynamic(() => import("./components/HeightMesh"), { ssr: false });
-const HeightMeshRGB24 = dynamic(() => import("./components/HeightMeshRGB24"), { ssr: false });
 const HeightMesh_Shaders = dynamic(() => import("./components/HeightMesh_Shaders"), { ssr: false });
 
 export default function Home() {
@@ -16,9 +13,6 @@ export default function Home() {
   return (
     <div style={{display: "flex", flexDirection: "column", width: "100%", height: "100vh"}}>
       <div style={{flex: "0 0 80%", position: "relative"}}>
-        {/* <CesiumMap datehour={datehour} /> */}
-        {/* <HeightMesh datehour={datehour} /> */}
-        {/* <HeightMeshRGB24 pngUrl={`http://localhost:8001/gph/${datehour}`} exaggeration={0.25} />; */}
         <HeightMesh_Shaders pressureLevel={250} datehour={datehour} pngUrl={`/api/gph/250/${datehour}`} landUrl={`/api/landmask`} uvUrl={`/api/uv/250/${datehour}`} exaggeration={0.25} />;
       </div>
       <div style={{flex: "0 0 20%", borderTop: "1px solid rgba(0,0,0,0.1)"}}>
